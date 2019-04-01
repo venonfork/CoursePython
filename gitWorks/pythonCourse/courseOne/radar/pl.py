9302ed4e10ba169dff59910ab5528e43155c53ab
Venun <venonfork@gmail.com>
2019-04-01 17:03:11 +0200

    test4

---
+++ gitWorks/pythonCourse/courseOne/radar/pl.py
@@ -0,0 +1,7 @@
+class pl:
+    def __init__(self, h, l):
+        self._h = 100 
+        self._l = 200
+
+    def planDessin(self, _h, _l):
+        return plan
---
+++ gitWorks/pythonCourse/courseOne/radar/ra.py
@@ -0,0 +1,25 @@
+class Ra:
+    def __init__(self, center, cercle, detection, dectType):
+        self.center = center
+        self.cercle = cercle
+        self.detection = detection
+        self.detectType = detectType
+        # self.point = point  
+        
+    def zone(self, i):
+        if ("centre" - "point object") > "rayonZone1":
+            return horsZone  
+        if ("centre" - "point object") < "rayonZone1" and ("centre" - "point object") <= "rayonZone1":
+            return zone1
+        if ("centre" - "point object") > "rayonZone2" and ("centre" - "point object") <= "rayonZone1":
+            return zone2
+        if ("centre" - "point object") < "rayonZone2" and ("centre" - "point object") <= "rayonZone2":
+            return zone3
+        return f" {rien} "
+
+    def dectTypeObjet(self):
+    #    // vitesse + position retour type d'appareil
+
+
+    
+    
